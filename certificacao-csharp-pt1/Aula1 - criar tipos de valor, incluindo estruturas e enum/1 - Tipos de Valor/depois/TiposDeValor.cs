9b905bcb49e6da247300ba193bf2dd256b2c4a7c
JhonReimberg <joaoreimberg09@gmail.com>
2022-01-11 17:06:20 -0300

    Adicionar arquivos de projeto.

---
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/depois/TiposDeValor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int tamanho;
+            tamanho = 3;
+            Console.WriteLine(tamanho);
+
+            int Peso;
+            Peso = 450006;
+            Console.WriteLine(Peso);
+
+            int CopiaTamanho = tamanho;
+            Console.WriteLine($"tamanho: {tamanho}");
+
+            Console.WriteLine($"CopiaTamanho: {CopiaTamanho}");
+            Console.WriteLine($"Peso: {Peso}");
+        } 
+        
+        
+
+    }
+}
